a6b5b981ff8512282e88bfff64aaa7735cbcd15c
Tajbir Sandhu <sandman.taj@gmail.com>
2020-02-17 17:37:02 -0800

    began implementing gallery scroll view

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,13 +1,55 @@
 import React from 'react';
-import { S3Album } from 'aws-amplify-react-native';
-import { View, StyleSheet } from 'react-native';
+import { S3Image } from 'aws-amplify-react-native';
+import { Storage } from 'aws-amplify';
+import { View, StyleSheet, Alert, ScrollView } from 'react-native';
+import FastImage from 'react-native-fast-image';
 
 export default class Gallery extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            image: '',
+        }
+        this.images = [];
+    }
+
+    async componentDidMount() {
+        await this.getList();
+        await this.getImage(0);
+    }
+
+    getImage = async(url) => {
+        const data = await Storage.get(url);
+        return data;
+    }
+
+    getList = async() => {
+        await Storage.list('', { level: 'public' })
+            .then((result) => {
+                for (var i in result) {
+                    this.images.push(result[i]['key']);
+                };
+            });
+    }
+
+    
+
     render() {
+        const list = this.images.map(image => {
+            const URI = this.getImage(image);
+            console.log(image);
+            return (
+                <FastImage
+                    style={{width: 500, height: 500}}
+                    source={{uri: URI}}
+                    />
+            )
+        })
         return(
-            <View style={styles.gallery_container}>
-                <S3Album path={''} picker />
-            </View>
+            <ScrollView>
+                {list}
+            </ScrollView>
         )
     }
 }
