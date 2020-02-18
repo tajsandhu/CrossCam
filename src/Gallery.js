cb86e37b512f6cf7911c267e0920b4bec20d0f27
Tajbir Sandhu <sandman.taj@gmail.com>
2020-02-17 19:27:43 -0800

    images now load into flat list

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { S3Image } from 'aws-amplify-react-native';
 import { Storage } from 'aws-amplify';
-import { View, StyleSheet, Alert, ScrollView } from 'react-native';
+import { View, StyleSheet, Alert, ScrollView, FlatList } from 'react-native';
 import FastImage from 'react-native-fast-image';
 
 export default class Gallery extends React.Component {
@@ -9,24 +9,40 @@ export default class Gallery extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            image: '',
+            images: '',
         }
         this.images = [];
+        this.list = [];
     }
 
     async componentDidMount() {
+        //gets the names off all the images
         await this.getList();
-        await this.getImage(0);
+        //gets a url for al of the images
+        const getList = async() => {
+            return Promise.all(this.images.map( image =>this.getImage(image)));
+        }
+        //appends these urls to a list
+        await getList().then(data => {
+            for (var d in data) {
+                this.list.push(data[d])
+            }
+            this.setState({images: this.list})
+        })
     }
 
-    getImage = async(url) => {
-        const data = await Storage.get(url);
+    //obtains a url for an image based on the name
+    getImage = async(name) => {
+        const data = await Storage.get(name);
         return data;
     }
 
+    //creates a list of the names of all images
     getList = async() => {
+        //returns a promise with a list of image names
         await Storage.list('', { level: 'public' })
             .then((result) => {
+                //appends the names to a list
                 for (var i in result) {
                     this.images.push(result[i]['key']);
                 };
@@ -36,20 +52,19 @@ export default class Gallery extends React.Component {
     
 
     render() {
-        const list = this.images.map(image => {
-            const URI = this.getImage(image);
-            console.log(image);
-            return (
-                <FastImage
-                    style={{width: 500, height: 500}}
-                    source={{uri: URI}}
-                    />
-            )
-        })
         return(
-            <ScrollView>
-                {list}
-            </ScrollView>
+            <View style={{flex: 1}}>
+                <FlatList
+                    data={this.state.images}
+                    renderItem={({item}) => (
+                        <FastImage
+                            style={{width: 500, height: 500}}
+                            source={{uri:item}}
+                        />
+                    )}
+                />
+            </View>
+
         )
     }
 }
